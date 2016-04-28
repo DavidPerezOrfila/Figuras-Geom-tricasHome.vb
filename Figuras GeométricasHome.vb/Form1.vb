d305aefb2078166c5bc15024993f4bf90d4b043f
David Pérez Orfila <knight_bcn@hotmail.com>
2016-04-28 23:06:36 +0200

    clase punto

---
+++ Figuras GeométricasHome.vb/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim p1 As punto = New punto
+
+        MessageBox.Show(p1.ToString)
+
+        Dim p2 As punto = New punto(1, 2)
+
+        MessageBox.Show(p2.ToString)
+
+        p1.setX(5)
+        p1.setY(9)
+
+        MessageBox.Show(p1.ToString)
+
+    End Sub
+
+
+End Class
---
+++ Figuras GeométricasHome.vb/Model/Punto.vb
@@ -0,0 +1,39 @@
+﻿Public Class punto
+    'Se instancian las variables private
+    Private x, y As Integer
+
+    'Se crean los constructores, valor por defecto y cogiendo resultados
+
+    Public Sub New()
+        Me.x = 0
+        Me.y = 0
+    End Sub
+
+    Public Sub New(ByVal x As Integer, ByVal y As Integer)
+        Me.x = x
+        Me.y = y
+
+    End Sub
+    'Se crean los getters y setters públicos
+    Public Function getX() As Integer
+        Return Me.x
+    End Function
+    Public Sub setX(ByVal x As Integer)
+        Me.x = x
+    End Sub
+    Public Function getY() As Integer
+        Return Me.y
+    End Function
+    Public Sub setY(ByVal y As Integer)
+        Me.y = y
+    End Sub
+    ' Se sobrescribe la funcion ToString()
+    Public Overrides Function ToString() As String
+        Return " Punto= {" + Me.x.ToString() + " ," + Me.y.ToString() + "}"
+    End Function
+
+    ' Devuelve la distancia entre esta instancia y  (0,0).
+    Public Overloads Function distancia() As Double
+        Return Math.Sqrt(((Me.x * Me.x) + (Me.y * Me.y)))
+    End Function
+End Class
